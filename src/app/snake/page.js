91e119c26d091101b1c8de7e2153a82bd450b521
Tuleniu <stecniki@gmail.com>
2024-06-06 20:30:51 +0300

    init -> snake

---
+++ src/app/snake/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Snake from "./Snake"
+
+const Page = () => {
+    return (
+        <div>
+            <Snake/>
+        </div>
+    );
+};
+
+export default Page;
